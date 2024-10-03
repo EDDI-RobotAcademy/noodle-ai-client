cc15abad7599d90196155f1dc086d7315328f2b9
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-03 20:10:23 +0900

    feat: [한재혁] 백로그 생성을 위해 깃허브 저장소를 로컬 PC에 저장소를 클론할 수 있습니다. [NAC-11]

---
+++ github_processing/repository/github_processing_repository_impl.py
@@ -0,0 +1,33 @@
+import os.path
+
+from git import Repo
+
+from github_processing.repository.github_processing_repository import GithubProcessingRepository
+
+
+class GithubProcessingRepositoryImpl(GithubProcessingRepository):
+    __instance = None
+    GITHUB_URL = "https://github.com"
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+
+        return cls.__instance
+
+    @classmethod
+    def getInstance(cls):
+        if cls.__instance is None:
+            cls.__instance = cls()
+
+        return cls.__instance
+
+    def cloneRespoitory(self, userName, githubRepositoryName):
+        GITHUB_REPOSITORY_URL = f"{self.GITHUB_URL}/{userName}/{githubRepositoryName}"
+        repositoryPath = f"./github_repositories/{githubRepositoryName}"
+
+        if os.path.exists(repositoryPath):
+            os.removedirs(repositoryPath)
+
+        Repo.clone_from(GITHUB_REPOSITORY_URL, to_path=repositoryPath)
+
