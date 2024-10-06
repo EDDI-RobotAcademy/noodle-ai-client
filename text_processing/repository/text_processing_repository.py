b73d51b61b7b6299ad66cccd158e2e46e6521b32
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-06 20:48:40 +0900

    feat: [한재혁] 클론한 깃허브 저장소 폴더를 순회하면서 파이썬 파일 내용을 하나의 텍스트로 합칩니다. [NAC-21]

--- text_processing/repository/text_processing_repository.py
+++ text_processing/repository/text_processing_repository.py
@@ -5,3 +5,7 @@ class TextProcessingRepository(ABC):
     @abstractmethod
     def postprocessingTextToBacklogs(self, generatedBacklogsText):
         pass
+
+    @abstractmethod
+    def getTextFromSourceCode(self, githubRepositoryPath):
+        pass
