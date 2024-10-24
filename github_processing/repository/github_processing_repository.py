4891a5601ec85fbd03261a2e046be088f913ced0
Jaehyuk Han <jh.jaehyuk213@gmail.com>
2024-10-24 17:45:40 +0900

    add: [한재혁] 깃허브 저장소의 브랜치를 특정해서 클론할 수 있도록 수정 [NAC-11]

--- github_processing/repository/github_processing_repository.py
+++ github_processing/repository/github_processing_repository.py
@@ -3,7 +3,7 @@ from abc import abstractmethod, ABC
 
 class GithubProcessingRepository(ABC):
     @abstractmethod
-    def cloneRepository(self, userName, githubRepositoryName):
+    def cloneRepository(self, userName, githubRepositoryName, githubBranchName):
         pass
 
     @abstractmethod
